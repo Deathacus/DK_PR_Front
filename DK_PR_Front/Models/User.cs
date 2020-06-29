cdbb8a937916c5d65a2b44837344e8151e52db51
JVfZNFcuTj@students.jku.at <JVfZNFcuTj@students.jku.at>
2020-06-29 19:21:52 +0200

    Neo4j-Anbindung

---
+++ DK_PR_Front/Models/User.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+namespace DK_PR_Front.Models
+{
+    public class User
+    {
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        //public int Id { get; set; }
+        //public int eMail { get; set; }
+    }
+}
---
+++ DK_PR_Front/Services/Neo4jService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using DK_PR_Front.Models;
+using DK_PR_Front.Neo4jConfig;
+using DK_PR_Front.Controllers;
+
+namespace DK_PR_Front.Services
+{
+    public class Neo4jService
+    {
+        Neo4jConfigClass neo4JConfig;
+        Neo4jController neo4JController;
+
+        public Neo4jService()
+        {
+            neo4JConfig = new Neo4jConfigClass("bolt://localhost:7687", "admin", "admin123");
+            neo4JController = new Neo4jController();
+        }
+        public void CreateUser(string username, string password)
+        {
+            neo4JController.CreateUserController(username, password);
+        }
+
+        public List<User> getUser()
+        {
+            return neo4JController.GetUserController();
+        }
+    }
+}
+

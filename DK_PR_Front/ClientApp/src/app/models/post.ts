84ed8ce818bdaca07e5ada9d18bda6fdd58e5107
Sara Scheucher <sara.scheucher@gmail.com>
2020-07-02 10:32:12 +0200

    setPost methode in Post.ts erstellt

--- DK_PR_Front/ClientApp/src/app/models/post.ts
+++ DK_PR_Front/ClientApp/src/app/models/post.ts
@@ -6,4 +6,13 @@
       public emojiName: string = "";
       public date: string = "";
       public postText: string = "";
+
+      public setPost(postId: number, userName: string, emojiName: string, date: string, postText: string) {
+        this.postId = postId;
+        this.userName = userName;
+        this.emojiName = emojiName;
+        this.date = date;
+        this.postText = postText;
+      }
+
     }
